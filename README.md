8e7324b03e189e26f41446177dc535c991a71a4d
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2018-04-29 12:20:11 +0300

    Add 2004 part2 - compo

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # console_js_demo
 
+## Part 2: composition
+
+Output:
+
+![](/part2.png)
+
 - [Part 1: stars](https://github.com/nikitinskaya/console_js_demo/tree/part1)
-- [Part 2: compo](https://github.com/nikitinskaya/console_js_demo/tree/part2)
 - [Part 3: rx](https://kodaktor.ru/rx3_cf4f8)
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello все люди!');
+const concat = require('goss_concat');
+
+//https://kodaktor.ru/j/goss_compose
+const compose = (...funcs) => funcs.reduce((accum, fn) => (...args) => accum(fn(...args), x => x));
+
+const s = 'abc';
+
+const up = x => String.prototype.toUpperCase.call(x);
+const ps = x => String.prototype.padStart.call(x, 10, '*');
+const rp = x => String.prototype.repeat.call(x, 2);
+const sp = x => String.prototype.split.call(x, '');
+const rv = x => Array.prototype.reverse.call(x);
+
+console.log(concat(...rv(sp(rp(ps(up(s)))))));
+console.log(concat(...compose(rv, sp, rp, ps, up)(s)));
